ce44926bbaa3446b2dc66012ef029f7e890e423b
Vladimir Komissarov <v.komissarov@zarplata.ru>
2018-03-18 01:24:37 +0700

    add access_token validation

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/reconquest/hierr-go"
 	"net/http"
+	"errors"
 )
 
 type Server struct {
@@ -28,6 +29,16 @@ func (proxy *Server) Run(addr ...string) {
 }
 
 func (proxy *Server) handleMessagesSend(ctx *gin.Context) {
+	accessToken := ctx.Query("access_token")
+	if accessToken == "" {
+		abort(
+			ctx,
+			http.StatusBadRequest,
+			errors.New("query `access_token` is required"),
+		)
+		return
+	}
+
 	request := ctx.Request
 	if err := request.ParseForm(); err != nil {
 		abort(
@@ -39,7 +50,7 @@ func (proxy *Server) handleMessagesSend(ctx *gin.Context) {
 	}
 	request.ParseMultipartForm(32 << 10) // 32 MB
 	proxy.commands <- VKCommand{
-		AccessToken: ctx.Query("access_token"),
+		AccessToken: accessToken,
 		Method:      fmt.Sprintf("API.%s", ctx.Param("name")),
 		Args:        request.Form,
 	}
@@ -53,7 +64,7 @@ func abort(ctx *gin.Context, code int, err error) {
 	ctx.Error(err)
 	ctx.JSON(code, gin.H{
 		"success": false,
-		"error":   err,
+		"error":   err.Error(),
 	})
 	ctx.Abort()
 }
